dbafd7b230f4c8410061113c724d5776ffd59bf7
crynobone <crynobone@gmail.com>
2014-09-30 18:35:02 +0800

    Use constructor parameters.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- libs/dbconnection.php
+++ libs/dbconnection.php
@@ -10,14 +10,14 @@ class dbconnection {
 
     public function __construct($host, $user, $password, $db) {
         $this->connection = new mysqli(
-            $this->host,
-            $this->user,
-            $this->passwd,
-            $this->db
+            $this->host = $host ?: $this->host,
+            $this->user = $user ?: $this->user,
+            $this->password = $password ?: $this->password,
+            $this->db = $db ?: $this->db
         );
 
         if ($this->connection->connect_error) {
-          trigger_error('Database connection failed: '  . $this->connection->connect_error, E_USER_ERROR);
+            trigger_error('Database connection failed: '  . $this->connection->connect_error, E_USER_ERROR);
         }
     }
 
